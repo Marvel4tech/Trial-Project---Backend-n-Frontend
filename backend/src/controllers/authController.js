da99bc04fecdf97b12e99abf159870379c22ac73
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-21 05:36:23 +0100

    register API is created

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,21 @@
+import User from "../models/userModel.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
 export const register = async (req, res) => {
-    res.status(200).json({ message: "Welcome to register page" })
+    const { username, email, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+        console.log( "New user : ", newUser )
+        res.status(201).json({ message: "User registered successfully", user: newUser })
+    } catch (error) {
+        res.status(500).json({ error: "Error registering user", message: error })
+    }
 }
 
 export const login = async (req, res) => {
